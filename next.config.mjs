db9a0603c21f132d0ffb174653366479e9ea4d08
digitalease031 <digitalease.product@gmail.com>
2025-07-23 12:08:21 -0700

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,24 +1,11 @@
 /** @type {import('next').NextConfig} */
-const isGithubActions = process.env.GITHUB_ACTIONS || false;
-
-let assetPrefix = '/mindcrypto';
-let basePath = '/mindcrypto';
-
-if (process.env.NODE_ENV === 'development') {
-  assetPrefix = '';
-  basePath = '';
-}
-
 const nextConfig = {
   output: 'export',
-  assetPrefix: assetPrefix,
-  basePath: basePath,
+  basePath: '/mindcrypto',
   images: {
     unoptimized: true,
-    path: `${assetPrefix}/_next/image`,
   },
-  // This is required for GitHub Pages
-  trailingSlash: true,
+  assetPrefix: '/mindcrypto/',
 };
 
 export default nextConfig;
