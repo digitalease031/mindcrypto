eab8f35b4b2397077b291c95b0cfb869b80f3f2e
digitalease031 <digitalease.product@gmail.com>
2025-07-23 11:50:05 -0700

    Update deployment configuration for mindcrypto-site

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/mindcrypto',
+  basePath: '/mindcrypto-site',
   images: {
     unoptimized: true,
     loader: 'custom',
     loaderFile: './src/utils/imageLoader.ts',
   },
   // This is required for GitHub Pages
-  assetPrefix: '/mindcrypto/',
+  assetPrefix: '/mindcrypto-site/',
 };
 
 export default nextConfig;
