1d64c187e5df91aaca3010d792aef083d1b22ca5
digitalease031 <digitalease.product@gmail.com>
2025-07-23 10:59:01 -0700

    Setup deployment to public repository

--- next.config.mjs
+++ next.config.mjs
@@ -1,24 +1,12 @@
 /** @type {import('next').NextConfig} */
-const isGithubActions = process.env.GITHUB_ACTIONS || false;
-
-let assetPrefix = '/mindcrypto';
-let basePath = '/mindcrypto';
-
-if (process.env.NODE_ENV === 'development') {
-  assetPrefix = '';
-  basePath = '';
-}
-
 const nextConfig = {
   output: 'export',
-  assetPrefix: assetPrefix,
-  basePath: basePath,
+  basePath: '/mindcrypto-site',
   images: {
     unoptimized: true,
-    path: `${assetPrefix}/_next/image`,
   },
   // This is required for GitHub Pages
-  trailingSlash: true,
+  assetPrefix: '/mindcrypto-site/',
 };
 
 export default nextConfig;
